82ae35cc620849363e71261353523c19a52fb72f
asosa2211 <lxs.22.lxs@gmail.com>
2026-05-22 08:12:18 -0400

    add VentaProducto.cs, PersistenciaVentasProductos.cs

---
+++ Cyberplay/Modelos/VentaProducto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cyberplay.Modelos
+{
+    public class VentaProducto
+    {
+        public Guid Id { get; set; }
+            = Guid.NewGuid();
+
+   
+        public string Producto { get; set; }
+
+     
+        public int Cantidad { get; set; }
+
+     
+        public decimal PrecioUnitario { get; set; }
+
+    
+        public decimal Total { get; set; }
+
+        public DateTime Fecha { get; set; } = DateTime.Now;
+
+        public string Cajero { get; set; }
+    }
+}
---
+++ Cyberplay/Persistencia/PersistenciaVentasProductos.cs
@@ -0,0 +1,38 @@
+﻿using Cyberplay.Modelos;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Cyberplay.Persistencia
+{
+    public class PersistenciaVentasProductos
+    {
+        private string ruta = "ventasProductos.json";
+
+    
+        public void GuardarVentas(List<VentaProducto> ventas)
+        {
+            string json = JsonConvert.SerializeObject(ventas, Formatting.Indented);
+
+            File.WriteAllText(ruta, json);
+        }
+
+   
+        public List<VentaProducto> CargarVentas()
+        {
+            if (!File.Exists(ruta))
+            {
+                return new List<VentaProducto>();
+            }
+
+            string json = File.ReadAllText(ruta);
+
+            return JsonConvert.DeserializeObject <List<VentaProducto> >(json) ??
+                   new List<VentaProducto>();
+        }
+    }
+}
