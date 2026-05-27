7b79ebf784ba38303c2bb19c35b36bee7df7b40d
asosa2211 <lxs.22.lxs@gmail.com>
2026-05-27 09:05:34 -0400

    frmHistorialCajas visual, OK.

--- Cyberplay/Formularios/frmHistorialCajas.Designer.cs
+++ Cyberplay/Formularios/frmHistorialCajas.Designer.cs
@@ -30,25 +30,30 @@
         {
             this.dgvHistorialCajas = new System.Windows.Forms.DataGridView();
             this.colNumeroCaja = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colCajero = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colApertura = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colCierre = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.colCajero = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colTotal = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dgvHistorialCajas)).BeginInit();
             this.SuspendLayout();
             // 
             // dgvHistorialCajas
             // 
+            this.dgvHistorialCajas.AllowUserToAddRows = false;
             this.dgvHistorialCajas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvHistorialCajas.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.colNumeroCaja,
+            this.colCajero,
             this.colApertura,
             this.colCierre,
-            this.colCajero,
             this.colTotal});
-            this.dgvHistorialCajas.Location = new System.Drawing.Point(31, 76);
+            this.dgvHistorialCajas.Location = new System.Drawing.Point(40, 36);
+            this.dgvHistorialCajas.MultiSelect = false;
             this.dgvHistorialCajas.Name = "dgvHistorialCajas";
-            this.dgvHistorialCajas.Size = new System.Drawing.Size(666, 150);
+            this.dgvHistorialCajas.ReadOnly = true;
+            this.dgvHistorialCajas.RowHeadersVisible = false;
+            this.dgvHistorialCajas.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvHistorialCajas.Size = new System.Drawing.Size(607, 232);
             this.dgvHistorialCajas.TabIndex = 0;
             this.dgvHistorialCajas.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvHistorialCajas_CellDoubleClick);
             // 
@@ -56,35 +61,44 @@
             // 
             this.colNumeroCaja.HeaderText = "Nro Caja";
             this.colNumeroCaja.Name = "colNumeroCaja";
-            // 
-            // colApertura
-            // 
-            this.colApertura.HeaderText = "Apertura";
-            this.colApertura.Name = "colApertura";
-            // 
-            // colCierre
-            // 
-            this.colCierre.HeaderText = "Cierre";
-            this.colCierre.Name = "colCierre";
+            this.colNumeroCaja.ReadOnly = true;
             // 
             // colCajero
             // 
             this.colCajero.HeaderText = "Cajero";
             this.colCajero.Name = "colCajero";
+            this.colCajero.ReadOnly = true;
+            // 
+            // colApertura
+            // 
+            this.colApertura.HeaderText = "Apertura";
+            this.colApertura.Name = "colApertura";
+            this.colApertura.ReadOnly = true;
+            this.colApertura.Width = 150;
+            // 
+            // colCierre
+            // 
+            this.colCierre.HeaderText = "Cierre";
+            this.colCierre.Name = "colCierre";
+            this.colCierre.ReadOnly = true;
+            this.colCierre.Width = 150;
             // 
             // colTotal
             // 
             this.colTotal.HeaderText = "Total";
             this.colTotal.Name = "colTotal";
+            this.colTotal.ReadOnly = true;
             // 
             // frmHistorialCajas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(688, 313);
             this.Controls.Add(this.dgvHistorialCajas);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "frmHistorialCajas";
-            this.Text = "frmHistorialCajas";
+            this.Text = "Historial de cajas";
             ((System.ComponentModel.ISupportInitialize)(this.dgvHistorialCajas)).EndInit();
             this.ResumeLayout(false);
 
@@ -94,9 +108,9 @@
 
         private System.Windows.Forms.DataGridView dgvHistorialCajas;
         private System.Windows.Forms.DataGridViewTextBoxColumn colNumeroCaja;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colCajero;
         private System.Windows.Forms.DataGridViewTextBoxColumn colApertura;
         private System.Windows.Forms.DataGridViewTextBoxColumn colCierre;
-        private System.Windows.Forms.DataGridViewTextBoxColumn colCajero;
         private System.Windows.Forms.DataGridViewTextBoxColumn colTotal;
     }
 }
