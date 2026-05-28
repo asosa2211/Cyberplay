7b47db6b184b1e984c3dfda530202921fdac9ba3
Carlos <lxs.22.lxs@gmail.com>
2026-05-27 20:14:44 -0400

    Agregado Rutas.cs

---
+++ Cyberplay/Helpers/Rutas.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cyberplay.Helpers
+{
+    public static class Rutas
+    {
+        // =====================
+        // BASE
+        // =====================
+
+        public static string Base =>
+            AppDomain
+            .CurrentDomain
+            .BaseDirectory;
+
+        // =====================
+        // DATA
+        // =====================
+
+        public static string Data =>
+            Path.Combine(
+                Base,
+                "Data");
+
+        // =====================
+        // API
+        // =====================
+
+        public static string API =>
+            Path.Combine(
+                Base,
+                "API");
+
+        // =====================
+        // WEB
+        // =====================
+
+        public static string Web =>
+            Path.Combine(
+                Base,
+                "Web");
+
+        // =====================
+        // TUNNEL
+        // =====================
+
+        public static string Tunnel =>
+            Path.Combine(
+                Base,
+                "Tunnel");
+    }
+}
--- Cyberplay/Program.cs
+++ Cyberplay/Program.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Cyberplay.Formularios;
+using System.IO;
+
+
 
 namespace Cyberplay
 {
