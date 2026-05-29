7b3a01ccc3678cf231a8308ee0ef8d5e88bf80c0
Carlos <lxs.22.lxs@gmail.com>
2026-05-29 19:06:08 -0400

    Agregada carpeta Sonidos, Sonidos.cs

---
+++ Cyberplay/Helpers/Sonidos.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cyberplay.Helpers
+{
+    public static class Sonidos
+    {
+        public static void Reproducir(
+            string archivo)
+        {
+            string ruta =
+                Path.Combine(
+                    Rutas.Root,
+                    "Sonidos",
+                    archivo);
+
+            if (!File.Exists(ruta))
+            {
+                return;
+            }
+
+            try
+            {
+                SoundPlayer player =
+                    new SoundPlayer(ruta);
+
+                player.Play();
+            }
+            catch
+            {
+            }
+        }
+    }
+}
--- Cyberplay/UC/ucPS4.cs
+++ Cyberplay/UC/ucPS4.cs
@@ -1983,6 +1983,7 @@ ActualizarUITransferida()
             AplicarColorTarifaSeleccionada();
 
             SonidoIniciar();
+           
 
             CentrarControl(
                 lblTiempoLimite);
